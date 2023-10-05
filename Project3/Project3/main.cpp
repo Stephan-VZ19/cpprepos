d0deb5e495aa7162f4de4b6a709e35f66cf99652
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-05 15:41:57 +0200

    shared_ptr

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -11,9 +11,19 @@ void foo(const Point& pIn, Point& pOut) {
 }
 
 int main() {
-	std::unique_ptr<Person> up = std::make_unique<Person>("Berta", 20);
+	std::shared_ptr<Person> up = std::make_shared<Person>("Berta", 20);
+	std::shared_ptr<Person> sp = std::make_shared<Person>("Paul", 22);
 
 	up->setAge(19);
+
+	std::cout << sp.use_count() << std::endl;
+
+	auto sp1 = sp;
+
+	std::cout << sp1.use_count() << std::endl;	// sp oder sp1 ist das gleiche
+
+	up->setAge(19);
+	sp->setAge(23);
 
 	std::cout << *up << std::endl;
 }
