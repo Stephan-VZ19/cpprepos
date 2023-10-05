f2bfbe2235964d706b3bfe70b7021664dd065009
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-05 14:49:31 +0200

    update bis referenzen

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -1,5 +1,6 @@
 #include "Point.h"
 #include "Person.h"
+#include <cassert>
 
 #include <iostream>
 
@@ -9,5 +10,27 @@ int main() {
 	Point p0;
 	Point p1(3);
 	Point p2(5, 6);
+
+	Point* pp0 = &p0;
+	auto pp01 = pp0;
+
+	Point p11 = p1;
+
+	assert(&p11 != &p1);
+
+	Point* pp2 = &p2;
+
+	auto d = pp2->dist(p2);
+
+
+
+	std::cout << "pp0 = " << pp0 << std::endl;
+	std::cout << "*pp0 = " << *pp0 << std::endl;
+
+	std::cout << "pp01 = " << pp01 << std::endl;
+	std::cout << "*pp01 = " << *pp01 << std::endl;
+
+	std::cout << "d = " << d << std::endl;
+
 
 }
