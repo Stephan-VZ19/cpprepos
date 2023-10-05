8f84d9954a9e62994a7efb9d7beec6d0ffa5b037
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-05 15:16:09 +0200

    update

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -11,26 +11,14 @@ int main() {
 	Point p1(3);
 	Point p2(5, 6);
 
-	Point* pp0 = &p0;
-	auto pp01 = pp0;
+	Point& rp1 = p1;
+	assert(&rp1 == &p1);
 
-	Point p11 = p1;
+	Point& rp11 = rp1;
 
-	assert(&p11 != &p1);
-
-	Point* pp2 = &p2;
-
-	auto d = pp2->dist(p2);
+	rp1 = p2;
 
 
 
-	std::cout << "pp0 = " << pp0 << std::endl;
-	std::cout << "*pp0 = " << *pp0 << std::endl;
-
-	std::cout << "pp01 = " << pp01 << std::endl;
-	std::cout << "*pp01 = " << *pp01 << std::endl;
-
-	std::cout << "d = " << d << std::endl;
-
-
+	std::cout << p1 << std::endl;
 }
