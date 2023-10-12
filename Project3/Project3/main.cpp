05df9073854485e011d2dc09e088192582630aae
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-12 15:28:32 +0200

    update before delete

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -2,10 +2,12 @@
 #include "Person.h"
 #include <cassert>
 #include <vector>
-
+#include <array>
 #include <iostream>
-
+#include <assert.h>
 #include "PointArray.h"
+#include <string_view>
+
 
 int Person::getAge() const { return m_age; }
 
@@ -18,11 +20,19 @@ int main() {
 	PointArray pa2(10);
 	Person person[] {Person("Peter", 20), Person("Moni", 21)};
 
+	std::array<Point, 3> a1{ Point(1,1), Point(2,3), Point(3,4)};
+	std::vector<Person> v1{ Person("Peter", 20), Person("Moni", 21) };
+
+	auto x = a1[1];		// Kopie
+	// assert(&x != a1[1]);
+
 	pa1.pushBack({ 3, 5 });
 	pa1.pushBack({ 7, 5 });
 
 	std::cout << pa1 << std::endl;
 	std::cout << pa2 << std::endl;
+
+
 
 	
 
