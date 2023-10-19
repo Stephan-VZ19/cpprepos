52f5f39f301206a19ea2a33d7f4e5d5f17c08bfe
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-19 14:34:24 +0200

    done enum project Datatyps

--- Datatypes/Datatypes/main.cpp
+++ Datatypes/Datatypes/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <cstdint>
+#include <cassert>
 
+enum class TicketStatus {
+	open, inProgress, done
+};
 
+enum class TaskStatus {
+	open, inProgress, review, done
+};
+
+std::ostream& operator<<(std::ostream& os, const TicketStatus& st) {
+	switch (st) {
+		case TicketStatus::open:
+			return os << "open";
+		case TicketStatus::inProgress:
+			return os << "inProgress";
+		case TicketStatus::done:
+			return os << "done";
+		default:
+			assert(false);
+	}
+}
 
 int main() {
-
+	TicketStatus st1 = (TicketStatus) 0;
+	st1 = TicketStatus::inProgress;
+	TaskStatus st2 = TaskStatus::open;
+	std::cout << st1 << std::endl;
+	std::cout << (int) st2 << std::endl;
 }
