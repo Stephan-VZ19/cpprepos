f9cfb7f86f910595adb88ef349d71a57cff46507
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-19 14:17:14 +0200

    init Datatyp project

--- Project5/Project5/main.cpp
+++ Project5/Project5/main.cpp
@@ -12,7 +12,23 @@ struct Chars {
 	char a, b, c, d;
 };
 
+struct Header {
+	uint64_t a;
+	uint32_t b;
+	uint64_t c;
+	uint16_t d;
+};	// 8 + 4 + 2 + 8 = 22
+
 int main() {
+	Bools example{ true, false, false, true };
+	std::cout << example.a << std::endl;
 	std::cout << "Bools belegen " << sizeof(Bools) << " Bytes" << std::endl;
-	std::cout << "Bools belegen " << sizeof(Chars) << " Chars" << std::endl;
+	std::cout << "Bools belegen " << sizeof(Chars) << " Bytes" << std::endl;
+
+	Header h;
+	std::cout << "Header belegen " << sizeof(h) << " Bytes" << std::endl;
+	std::cout << &h.a << std::endl;
+	std::cout << &h.b << std::endl;
+	std::cout << &h.c << std::endl;
+	std::cout << &h.d << std::endl;
 }
