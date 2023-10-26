c84c8da266583136503d1dda729f0ec39b8bf499
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-26 13:53:40 +0200

    l- r- values

--- Move/Move/main.cpp
+++ Move/Move/main.cpp
@@ -3,14 +3,33 @@
 
 using namespace std;
 
-void print(vector<int>& vec) {
+void print(const vector<int>& vec) {
 	for (int elem : vec) {
 		cout << elem << ' ';
 	}
 	cout << endl;
 }
 
+void test(int& x) {
+	cout << "non-moveable" << endl;
+}
+
+void test(int&& x) {
+	cout << "moveable" << endl;
+}
+
+
 int main() {
 	vector<int> v1{ 1, 2, 3, 4 };
 	print(v1);
+	print({ 1, 2, 3 });		// in/out object
+
+	int x = 5, y = 3;
+	int* ptr = &y;
+	test(x);	// l value
+	test(5);	// r value
+	test(x + 5);	// r value
+	test(*ptr);		// l value
+
+
 }
