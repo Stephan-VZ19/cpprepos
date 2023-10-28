8963f61cf4611827f2acd1b8791c5ad308419562
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-10-28 19:34:58 +0200

    Aufgabe 2b done

--- Uebung1/source/src/histogram.cpp
+++ Uebung1/source/src/histogram.cpp
@@ -27,12 +27,12 @@ void Histogram::process(const RGBImage& img, int numBins) {
     mData = make_unique<int[]>(numBins);
     // TODO [Aufgabe] 2.b)
 
-    double bins = 1.0 / numBins;        // range of 1 bins
+    // double bins = 1.0 / numBins;        // range of 1 bins
 
     for (int y = 0; y < img.getHeight(); y++) {
         for (int x = 0; x < img.getWidth(); x++) {
-            if (img.getPixel(x, y).getBrightness() < 1 - bins) {
-                int bClass = (int) (img.getPixel(x, y).getBrightness() / bins);     // which bins class the pixel should be
+            if (img.getPixel(x, y).getBrightness() < 1 - 1 / numBins) {
+                int bClass = (int) (img.getPixel(x, y).getBrightness() * numBins);     // which bins class the pixel should be
                 mData[bClass]++;                  
             }
             else {
@@ -47,29 +47,29 @@ void Histogram::print(int height) const {
     // TODO [Aufgabe] 2.c)
     // cout << "❚";
 
-    // search highest value
-    int posHighest = 0;
-    for (int i = 1; i < mNumBins; i++) {
-        if (mData[posHighest] < mData[i]) {
-            posHighest = i;
-        }
-    }
+    //// search highest value
+    //int posHighest = 0;
+    //for (int i = 1; i < mNumBins; i++) {
+    //    if (mData[posHighest] < mData[i]) {
+    //        posHighest = i;
+    //    }
+    //}
 
-    double blockSize = (mData[posHighest] * 1.0) / height;
+    //double blockSize = (mData[posHighest] * 1.0) / height;
 
-    // print each line, if each bins has enough value like i, print a block
-    for (int i = 0; i < height; i++) {
-        cout << "❚";
-        for (int j = 0; j < mNumBins; j++) {
-            if (mData[j] >= mData[posHighest] - i * blockSize) {
-                cout << "❚";
-            }
-            else {
-                cout << " ";
-            }
-            cout << endl;
-        }
-    }
+    //// print each line, if each bins has enough value like i, print a block
+    //for (int i = 0; i < height; i++) {
+    //    cout << "❚";
+    //    for (int j = 0; j < mNumBins; j++) {
+    //        if (mData[j] >= mData[posHighest] - i * blockSize) {
+    //            cout << "❚";
+    //        }
+    //        else {
+    //            cout << " ";
+    //        }
+    //        cout << endl;
+    //    }
+    //}
 
     cout << "|";
     for (int i = 1; i < mNumBins-1; i++) {
