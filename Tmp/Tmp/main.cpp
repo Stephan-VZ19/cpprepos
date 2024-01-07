75cdc0acc00b8ab6328a321342b257b10ca172e9
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2024-01-08 00:10:17 +0100

    testing purposes

--- Tmp/Tmp/main.cpp
+++ Tmp/Tmp/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stack>
 
 int main() {
 	std::cout << "Hallo" << std::endl;
@@ -14,5 +15,11 @@ int main() {
 
 	std::cout << b << std::endl;
 
+	std::stack<std::string> stk;
+	stk.push("2");
+
+	auto t = stk.top();
+
+	std::cout << t << std::endl;
 
 }
