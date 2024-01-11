8d1f9bd5eefeaa1090529c5141163a3d553d320a
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2024-01-11 15:22:29 +0100

    update correct until use of count()

--- FunctionalCpp/FunctionalCpp/main.cpp
+++ FunctionalCpp/FunctionalCpp/main.cpp
@@ -11,12 +11,37 @@ AT accumulate(Iter beg, Iter end, AT a) {
 	return a;
 }
 
+template <class Iter, typename AT, typename Func>
+AT accumulate(Iter beg, Iter end, AT a, Func f) {
+	while (beg != end) {
+		a = f(a, *beg++);
+	}
+	return a;
+}
+
+template<class Iter, class Pred>
+size_t count(Iter b, Iter e, Pred p) {
+	size_t cnt = 0;
+	while (b != e) {
+		cnt += p(*b++);
+	}
+	return cnt;
+}
+
 int main() {
 
 	std::vector<int> v = { 1, 2, 3, 4, 5 };
-
+	
 	int x = accumulate(v.cbegin(), v.cend(), 0);
+	int y = accumulate(v.cbegin(), v.cend(), 1, [] (int a, int x) {
+		return a *= x;
+		});
 
 	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+
+	/*size_t b = count(v.cbegin(), v.cend(), [] (int a, int x) {
+		return a & 2 == 0;
+		});*/
 
 }
